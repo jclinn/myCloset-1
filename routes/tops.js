d952970cf2d06a050c90eb25315a5b3270347d58
vagrant <vagrant@precise32.(none)>
2014-02-13 09:25:25 +0000

    hecka files

--- routes/tops.js
+++ routes/tops.js
@@ -1,10 +1,10 @@
-var data = require("../tops.json");
+var tops = require("../tops.json");
 
 exports.view = function(req, res){
   res.render("tops");
 };
 
 exports.getTops = function(req, res){
-	res.render( 'tops', { "tops_list" : data} );
+	res.render( 'tops', { "tops_list" : tops } );
 };
 

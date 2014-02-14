530d0555cdc0f150ed670687c0c5cedc79b17676
vagrant <vagrant@precise32.(none)>
2014-02-14 03:43:28 +0000

    view closet now parses json

--- routes/viewcloset.js
+++ routes/viewcloset.js
@@ -1,4 +1,7 @@
 var tops = require("../tops.json");
+var pants = require("../pants.json");
+var shoes = require("../shoes.json");
+var extra = require("../extra.json");
 
 exports.view = function(req, res){
   res.render('viewcloset');
@@ -7,5 +10,10 @@ exports.view = function(req, res){
 
 exports.getCloset = function(req, res){
 	console.log("where are you?");
-	res.render( 'viewcloset', { "viewclo_list" : tops } );
+	res.render( 'viewcloset', { 
+  	"makeo_list" : tops,
+	"pants_list" : pants,
+	"shoes_list" : shoes,
+	"extra_list" : extra	
+		} );
 };

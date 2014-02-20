248c67ee8dced6dbb188f623b3b58c3e3c8ec08b
vagrant <vagrant@precise32.(none)>
2014-02-20 07:34:56 +0000

    signup connects to db

--- models.js
+++ models.js
@@ -3,10 +3,52 @@ var Mongoose = require('mongoose');
 
 
 var UserSchema = new Mongoose.Schema({
+	"first": String,
+	"last" : String,
   	"email": String,
 	"password": String
 });
 
 exports.User = Mongoose.model('User', UserSchema);
 
+var TopSchema = new Mongoose.Schema ({
+	"url": String
+});
 
+exports.Top = Mongoose.model('Top', TopSchema);
+
+var PantSchema = new Mongoose.Schema ({
+	"url": String
+});
+
+exports.Pant = Mongoose.model('Pant', PantSchema);
+
+var ShoeSchema = new Mongoose.Schema ({
+	"url": String
+});
+
+exports.Shoe = Mongoose.model('Shoe', ShoeSchema);
+
+var ExtraSchema = new Mongoose.Schema ({
+	"url": String
+});
+
+exports.Extra = Mongoose.model('Extra', ExtraSchema);
+
+var CategorySchema = new Mongoose.Schema ({
+	"category": String
+});
+
+exports.Category = Mongoose.model('Category', CategorySchema);
+
+var OutfitSchema = new Mongoose.Schema({
+	"topurl": String,
+	"panturl": String,
+	"shoeurl": String,
+	"extraurl": String,
+	"category1": String,
+	"category2": String,
+	"category3": String
+});
+
+exports.Outfit = Mongoose.model('Outfit', OutfitSchema);
--- routes/signup.js
+++ routes/signup.js
@@ -1,3 +1,17 @@
+var models = require('../models');
+
 exports.view = function(req, res){
   res.render('signup');
+};
+
+exports.signup = function(req, res){
+	var form_data = req.body;
+	console.log(form_data);
+
+	var newUser = new models.User(form_data);
+
+	newUser.save(function(err) {
+		if(err) console.log(err);
+		res.send();
+	});
 };

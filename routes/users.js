d8d80488aa2ec37d2f8c4ec44cc17b4334b8d9ed
vagrant <vagrant@precise32.(none)>
2014-02-20 02:20:03 +0000

    set up db

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+
+var models = require('../models');
+
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+
+  models.User
+    .find()
+    .exec(renderUsers);
+
+  function renderUsers(err, users) {
+    console.log("are you rendering?");
+    res.render('users', { 'users': users });
+  }
+
+};

8b5a0866ae7e206f8e9c497d1da32c2acd1b61fc
jclinn <judyclinn@gmail.com>
2014-03-03 05:35:20 +0000

    J: setting & signup fixes

--- public/js/signup.js
+++ public/js/signup.js
@@ -11,54 +11,6 @@ $(document).ready(function() {
  */
 function initializePage() {
 
-	/*$('.project a').click(function(e) {
-		// Prevent following the link
-		e.preventDefault();
-
-		// Get the div ID, e.g., "project3"
-		var projectID = $(this).closest('.project').attr('id');
-		// get rid of 'project' from the front of the id 'project3'
-		var idNumber = projectID.substr('project'.length);
-
-		// this is the URL we'll call
-		var url_call = '/project/'+idNumber;
-
-		// How to respond to the GET request
-		function addProjectDetails(project_json) {
-			// We need to compute a display string for the date
-			// Search 'toLocaleDateString' online for more details.
-			var date_obj = new Date(project_json['date']);
-			var options = {
-				weekday: "long",
-				year: "numeric",
-				month: "long",
-				day: "numeric"
-			};
-			var display_date = date_obj.toLocaleDateString('en-US', options);
-
-			// compose the HTML
-			var new_html =
-				'<div class="project-date">'+display_date+'</div>'+
-				'<div class="project-summary">'+project_json['summary']+'</div>'+
-				'<button class="project-delete btn btn-default" '+
-					'type="button">delete</button>';
-
-			// get the DIV to add content to
-			var details_div = $('#project' + idNumber + ' .details');
-			// add the content to the DIV
-			details_div.html(new_html);
-
-			details_div.find('.project-delete').click(function(e) {
-				$.post('/project/'+idNumber+'/delete', function() {
-					window.location.href = '/';
-				});
-			});
-		}
-
-		// issue the GET request
-		$.get(url_call, addProjectDetails);
-	});*/
-
 	$('#signupbt').click(function(e) {
 		console.log('clicked');
 
@@ -85,10 +37,17 @@ function initializePage() {
 	     }
 	              
 	     if(valid){
+	     	if(pw.length < 1) {
+	     		alert("Please enter a password");
+	     		setTimeout("document.signupForm.pword.focus()", 25);
+         		setTimeout("document.signupForm.pword.select()", 25);
+	     	}
+	     	else {
 			 $.post('/signup/new', json, function() {
 			 	localStorage.setItem("user", email);
 				window.location.href = '/intro'; // reload the page
 			});
+			}
 		 	return false;
 	     }
 	     else{
